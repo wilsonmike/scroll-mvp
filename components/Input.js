f577622dc1244e0e7135b5105e5f2dfa26d67ea9
mike <michaelwilsonsj@hotmail.com>
2022-05-30 20:34:16 -0400

    added scrollbar hide && textarea for tweet

--- components/Input.js
+++ components/Input.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Input = () => {
+  const [input, setInput] = useState("");
   return (
-    <div>
-        <img src="https://lh3.googleusercontent.com/a/AATXAJwCsuneWAkKlHwMPx0mLNjFACEvbtN8QPwbUsZ-=s96-c" className='h-10 w-10 rounded-full xl:mr-2.5' alt="" />
+    <div className='border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll scrollbar-hide'>
+        <img src="https://lh3.googleusercontent.com/a/AATXAJwCsuneWAkKlHwMPx0mLNjFACEvbtN8QPwbUsZ-=s96-c" className='h-11 w-11 rounded-full cursor-pointer' alt="" />
+    <div className='w-full divide-y divide-gray-700'>
+        <div className={``}>
+            <textarea 
+            value={input} 
+            rows="2" 
+            placeholder="What's happening?" 
+            className='bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full'
+            onChange={(e) => setInput(e.target.value)}
+            ></textarea>
+        </div>
+    </div> 
     </div>
   )
 }
