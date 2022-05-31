071f4282169bc8a0eb4db90598e7f0b7b4c88b33
mike <michaelwilsonsj@hotmail.com>
2022-05-30 20:52:27 -0400

    setting file state && ed

--- components/Input.js
+++ components/Input.js
@@ -2,6 +2,7 @@ import {useState} from 'react'
 
 const Input = () => {
   const [input, setInput] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
   return (
     <div className='border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll scrollbar-hide'>
         <img src="https://lh3.googleusercontent.com/a/AATXAJwCsuneWAkKlHwMPx0mLNjFACEvbtN8QPwbUsZ-=s96-c" className='h-11 w-11 rounded-full cursor-pointer' alt="" />
@@ -11,9 +12,14 @@ const Input = () => {
             value={input} 
             rows="2" 
             placeholder="What's happening?" 
-            className='bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full'
+            className='bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]'
             onChange={(e) => setInput(e.target.value)}
-            ></textarea>
+            />
+            <div className='relative'>
+                <div>
+
+                </div>
+            </div>
         </div>
     </div> 
     </div>
