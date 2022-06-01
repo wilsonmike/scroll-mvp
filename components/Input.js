5e878bc9dd60c4beabe64e6431af8c50008fd100
mike <michaelwilsonsj@hotmail.com>
2022-05-31 21:54:25 -0400

    adding icons to input mponent

--- components/Input.js
+++ components/Input.js
@@ -24,12 +24,15 @@ const Input = () => {
             />
             {selectedFile && (
             <div className='relative'>
-                <div className='absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer'>
+                <div className='absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer' onClick={() => setSelectedFile(null)}>
                   <XIcon className='text-white h-5' />
                 </div>
                 <img src={selectedFile} alt="" className='rounded-2xl max-h-80 object-contain'/>
             </div>
             )}
+        </div>
+        <div className="flex items-center justify-between pt-2 5">
+
         </div>
     </div> 
     </div>
