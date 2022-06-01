f95b5ed9c84a105a9a037d1908e5e262a41e4347
mike <michaelwilsonsj@hotmail.com>
2022-05-31 21:26:38 -0400

    input selected file condition && added xicon

--- components/Input.js
+++ components/Input.js
@@ -1,4 +1,11 @@
 import {useState} from 'react'
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+  PhotographIcon,
+  XIcon,
+} from "@heroicons/react/outline";
 
 const Input = () => {
   const [input, setInput] = useState("");
@@ -15,11 +22,14 @@ const Input = () => {
             className='bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]'
             onChange={(e) => setInput(e.target.value)}
             />
+            {selectedFile && (
             <div className='relative'>
-                <div>
-
+                <div className='absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer'>
+                  <XIcon className='text-white h-5' />
                 </div>
+                <img src={selectedFile} alt="" className='rounded-2xl max-h-80 object-contain'/>
             </div>
+            )}
         </div>
     </div> 
     </div>
